50c2ff1112a7b3d2ef41b0f0dd36538864179b0b
Mustapha Younow <donotdo866@gmail.com>
2022-09-10 23:30:03 +0300

    Add project files.

---
+++ mustafa book web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mustafa_book_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
